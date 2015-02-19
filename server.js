fe0d4792cbc2b531ba826fe337f4a920bc735628
StephanieBeaton <stefanybeaton@gmail.com>
2015-02-19 11:36:32 -0800

    coded and tested -- Express REST API for single resource

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var express = require('express');
+var mongoose = require('mongoose');
+var platypusRoutes = require('./routes/platypus_routes');
+
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/platypusapp_development');
+
+var app = express();
+var router = express.Router();
+
+platypusRoutes(router);
+
+app.use('/api/v1', router);
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('server listening on port ' + (process.env.PORT || 3000));
+});
